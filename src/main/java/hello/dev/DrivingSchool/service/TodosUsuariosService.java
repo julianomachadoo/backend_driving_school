113301d57b4414a337bfe141cfce68ce6ebe27fa
julianomachadoo <julmachadoramos@gmail.com>
2023-01-29 16:13:20 -0300

    refactor/usuarioService

--- src/main/java/hello/dev/DrivingSchool/service/TodosUsuariosService.java
+++ src/main/java/hello/dev/DrivingSchool/service/TodosUsuariosService.java
@@ -1,6 +1,7 @@
 package hello.dev.DrivingSchool.service;
 
 import hello.dev.DrivingSchool.exceptions.DadosNaoEncontradosException;
+import hello.dev.DrivingSchool.model.Aluno;
 import hello.dev.DrivingSchool.model.Usuario;
 import hello.dev.DrivingSchool.rest.dto.UsuarioDTO;
 import hello.dev.DrivingSchool.rest.form.CadastroDeUsuarioForm;
@@ -9,12 +10,22 @@ import org.springframework.stereotype.Service;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TodosUsuariosService extends UsuarioService{
+
     @Override
     public Usuario cadastrar(CadastroDeUsuarioForm cadastroDeUsuarioForm) {
         return null;
+    }
+
+    public List<UsuarioDTO> listarTodos() {
+        return usuarioRepository.findAll().stream().map(this::converterUsuario).collect(Collectors.toList());
+    }
+
+    public List<UsuarioDTO> pesquisaPorNome(String nome) {
+        return usuarioRepository.findByNomeContainingIgnoreCase(nome).stream().map(this::converterUsuario).collect(Collectors.toList());
     }
 
     public List<UsuarioDTO> pesquisaPorCPF(String cpf) {
@@ -23,4 +34,22 @@ public class TodosUsuariosService extends UsuarioService{
         return Collections.singletonList(converterUsuario(usuario.get()));
     }
 
+    protected UsuarioDTO converterUsuario (Usuario usuario) {
+        UsuarioDTO usuarioDTO = new UsuarioDTO(
+                usuario.getId(),
+                usuario.getNome(),
+                usuario.getEmail(),
+                usuario.getSenha(),
+                usuario.getCpf(),
+                usuario.getDataDeNascimento(),
+                usuario.getDataCadastro(),
+                usuario.getEndereco(),
+                usuario.getTelefone()
+        );
+
+        if (usuario.getClass().equals(Aluno.class)) {
+            ((Aluno) usuario).getTipoCNHList().forEach(tipoCNH -> usuarioDTO.getTipoCNHList().add(tipoCNH));
+        }
+        return usuarioDTO;
+    }
 }
--- src/main/java/hello/dev/DrivingSchool/service/UsuarioService.java
+++ src/main/java/hello/dev/DrivingSchool/service/UsuarioService.java
@@ -1,16 +1,12 @@
 package hello.dev.DrivingSchool.service;
 
-import hello.dev.DrivingSchool.model.Aluno;
 import hello.dev.DrivingSchool.model.Usuario;
 import hello.dev.DrivingSchool.repository.UsuarioRepository;
-import hello.dev.DrivingSchool.rest.dto.UsuarioDTO;
 import hello.dev.DrivingSchool.rest.form.AtualizaUsuarioForm;
 import hello.dev.DrivingSchool.rest.form.CadastroDeUsuarioForm;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.lang.Integer.parseInt;
 
@@ -20,10 +16,6 @@ public abstract class UsuarioService {
     UsuarioRepository usuarioRepository;
 
     public abstract Usuario cadastrar(CadastroDeUsuarioForm cadastroDeUsuarioForm);
-
-    public List<UsuarioDTO> pesquisaPorNome(String nome) {
-        return usuarioRepository.findByNomeContainingIgnoreCase(nome).stream().map(this::converterUsuario).collect(Collectors.toList());
-    }
 
     public void atualizaUsuario(AtualizaUsuarioForm atualizaUsuarioForm, String id) {
         Usuario usuarioEncontrado = usuarioRepository.getReferenceById(id);
@@ -42,29 +34,6 @@ public abstract class UsuarioService {
     protected LocalDate converterData (String data) {
         String[] split = data.split("-");
         return LocalDate.of(parseInt(split[2]), parseInt(split[1]), parseInt(split[0]));
-    }
-
-    protected UsuarioDTO converterUsuario (Usuario usuario) {
-        UsuarioDTO usuarioDTO = new UsuarioDTO(
-                usuario.getId(),
-                usuario.getNome(),
-                usuario.getEmail(),
-                usuario.getSenha(),
-                usuario.getCpf(),
-                usuario.getDataDeNascimento(),
-                usuario.getDataCadastro(),
-                usuario.getEndereco(),
-                usuario.getTelefone()
-        );
-
-        if (usuario.getClass().equals(Aluno.class)) {
-            ((Aluno) usuario).getTipoCNHList().forEach(tipoCNH -> usuarioDTO.getTipoCNHList().add(tipoCNH));
-        }
-        return usuarioDTO;
-    }
-
-    public List<UsuarioDTO> listarTodos() {
-        return usuarioRepository.findAll().stream().map(this::converterUsuario).collect(Collectors.toList());
     }
 }
 
